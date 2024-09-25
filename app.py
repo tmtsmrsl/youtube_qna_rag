e4234680ba8e15ef22e2e581945ff6f68833a5fa
tmtsmrsl <timotiusmarselo@gmail.com>
2024-09-25 19:59:43 +1200

    change llm provider, add embedding provider

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
+import os
+
 import chainlit as cl
 from dotenv import load_dotenv
-from utils import load_llm, load_vector_store, create_qa_chain
-import os
+
+from utils import create_qa_chain, load_llm, load_vector_store
+
 
 @cl.on_chat_start
 async def on_chat_start():
     load_dotenv()
-    pinecone_api_key = os.getenv("PINECONE_API_KEY")
-    fireworks_api_key = os.getenv("FIREWORKS_API_KEY")
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    DEEPINFRA_API_KEY = os.getenv("DEEPINFRA_API_KEY")
     
-    vector_store = load_vector_store(pinecone_api_key, local_embeddings=True)
-    llm = load_llm(fireworks_api_key)
+    vector_store = load_vector_store(PINECONE_API_KEY, DEEPINFRA_API_KEY, local_embeddings=False)
+    llm = load_llm(GROQ_API_KEY)
     chain = create_qa_chain(llm, vector_store)
     
-    msg = cl.Message(content="Please ask me a question about machine learning!")
+    msg = cl.Message(content="Welcome to the StatQuest ML Video Search Assistant! Ask me any questions about machine learning, and I’ll help you find relevant StatQuest videos along with their timestamps.")
     await msg.send()
     
     cl.user_session.set("chain", chain)
@@ -23,15 +27,15 @@ async def main(message: cl.Message):
     chain = cl.user_session.get("chain") 
 
     response = chain.invoke({"input":message.content})
-    answer_src = response["answer"]
-    answer = answer_src.answer
-    source_documents = answer_src.sources
+    answer_and_src = response["answer"]
+    answer = answer_and_src.answer
+    source_documents = answer_and_src.sources
     formatted_sources = "\n".join(source_documents)
     
     if source_documents:
         elements = [
-            cl.Text(name="Sources", content=formatted_sources, display="inline"),
             cl.Video(url=source_documents[0], display="inline"),
+            cl.Text(name="Sources", content=formatted_sources, display="inline")
         ]
     else:
         elements = []
